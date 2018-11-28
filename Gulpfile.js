dbd87c87d7c57838dbb419c3cc8dee7559cf9040
vladimirzoyan <vladimirzoyan@gmail.com>
2018-11-28 08:14:59 -0500

    Rework with grid and mixing,functions,placeholders

--- Gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-
-gulp.task('styles', function() {
-    gulp.src('styles/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css/'));
-});
